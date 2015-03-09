b66ddb61a57b24350a4cd3aa486c4f7bf413c49f
GENIE DIGITAL AGENCY <eulergrj@hotmail.com>
2015-03-09 00:01:03 -0300

    teste
    teste

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="author" content="Genie Digital">
+	<link rel="author" href="http://geniedigital.com.br/">
+	<base href="" />
+	<title> Genie Digital Agency </title>
+	<meta name="description" content="Agência de desenvolvimento digital de qualidade com um toque de gênio!">
+	<meta name="keywords" content="" />
+
+	<link rel="shortcut icon" href="">
+	<link rel="apple-touch-icon" href="">
+	<link rel="stylesheet" href="public/css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
+
+	<script src="public/js/jquery-1.11.2.min.js"></script>
+	<script src="public/js/script.js"></script>
+
+</head>
+
+<body>
+
+<header>
+	<div class="hero">
+		<div class="wrapHero">
+			<div class="left" id="logo">
+				<h1><img src="public/images/logo.png"></h1>
+			</div>
+
+			<nav>
+				<ul class="right">
+					<li class="left navLink"><a href="#">Home</a></li>
+					<li class="left navLink"><a href="#">Sobre</a></li>
+					<li class="left navLink"><a href="#">Serviços</a></li>
+					<li class="left navLink"><a href="#">Orçamentos</a></li>
+					<li class="left "><a href="#">Contato</a></li>
+					<li class="left navLang"><a href="#">[PT]</a></li>
+					<li class="left "><a href="#">[EN]</a></li>
+				</ul>
+			</nav>
+			<div class="heroContent">
+				<div class="heroBorder">
+					<img src="public/images/lamp.png">
+				</div><!--heroBorder-->
+				<div class="heroText">
+					<h2>Onde seus <a>desejos</a> <p>se tornam <a>realidade</a></p></h2>
+				</div><!--heroText-->
+				<div class="heroButton">
+					<a href="#">Seu desejo é uma ordem!</a>
+				</div><!--heroButton-->
+			</div><!--heroContent-->
+		</div><!--wrapHero-->
+	</div><!--hero-->
+</header>
+
+<div id="container clear">
+	<div id="content">
+		<div class="homeAbout align">
+			<h3>Desenvolvimento de qualidade com um toque de gênio</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus molestie sit amet ipsum nec iaculis. Proin vestibulum volutpat libero, a blandit purus imperdiet nec. Vestibulum at lacus ipsum.</p>
+			<p class="left">Vivamus molestie sit amet ipsum nec iaculis. Proin vestibulum volutpat libero, a blandit purus imperdiet nec. Vestibulum at lacus ipsum.</p>
+			<p class="right">Vivamus molestie sit amet ipsum nec iaculis. Proin vestibulum volutpat libero, a blandit purus imperdiet nec. Vestibulum at lacus ipsum. Blandit purus imperdiet nec. Vestibulum at lacus ipsum.<br><a href="#">Saiba mais sobre a Genie aqui!</a></p>
+		</div><!--homeAbout-->
+		<div class="homeServices align clear">
+			<h3>Costumamos realizar desejos nas seguintes áreas:</h3>
+			<ul>
+				<li class="left">
+					<img class="left" src="public/images/services_icon_01.png">
+					<h4 class="right">Desenvolvimento web</h4>
+					<p>Consectetur adipiscing elit. Mauris mattis 
+					bibendum lect us ac aliquet. Ut blandit metus dui, eu convallis metus cursus eu. 
+					</p>
+				</li>
+				<li class="left" >
+					<img class="left" src="public/images/services_icon_02.png">
+					<h4 class="right">Identidade Visual</h4>
+					<p>Consectetur adipiscing elit. Mauris mattis 
+					bibendum lect us ac aliquet. Ut blandit metus dui, eu convallis metus cursus eu. 
+					</p>
+				</li>
+				<li class="left final">
+					<img class="left" src="public/images/services_icon_03.png">
+					<h4 class="right">Design de interfaces</h4>
+					<p>Consectetur adipiscing elit. Mauris mattis 
+					bibendum lect us ac aliquet. Ut blandit metus dui, eu convallis metus cursus eu. 
+					</p>
+				</li>
+				<li class="left">
+					<img class="left" src="public/images/services_icon_04.png">
+					<h4 class="right">Design responsivo</h4>
+					<p>Consectetur adipiscing elit. Mauris mattis 
+					bibendum lect us ac aliquet. Ut blandit metus dui, eu convallis metus cursus eu. 
+					</p>
+				</li>
+				<li class="left" >
+					<img class="left" src="public/images/services_icon_05.png">
+					<h4 class="right">Papelaria</h4>
+					<p>Consectetur adipiscing elit. Mauris mattis 
+					bibendum lect us ac aliquet. Ut blandit metus dui, eu convallis metus cursus eu. 
+					</p>
+				</li>
+				<li class="left final">
+					<img class="left" src="public/images/services_icon_06.png">
+					<h4 class="right">Design digital</h4>
+					<p>Consectetur adipiscing elit. Mauris mattis 
+					bibendum lect us ac aliquet. Ut blandit metus dui, eu convallis metus cursus eu. 
+					</p>
+				</li>
+			</ul>
+			<div class="servicesBottom clear">
+				<p><a>E não para por ai...</a></p>
+				<p>Faça seu pedido que nós o realizaremos.</p>	
+			</div><!--servicesBottom-->
+		</div><!--homeServices-->
+	</div><!--content-->
+</div><!--container-->
+
+<footer>
+</footer>
+
+</body>
+</html>
